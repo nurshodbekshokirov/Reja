d59ad9c4ec532cce1556fbcf8fe4fdb5528f9539
Your Name <nurshodbekshokirov@gmail.com>
2023-02-11 18:19:37 +0500

    Models yangilandi

---
+++ asosiy/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Reja(models.Model):
+    sarlavha = models.CharField(max_length=60)
+    vaqt = models.TimeField()
+    batafsil = models.TextField()
+    holat = models.BooleanField()
