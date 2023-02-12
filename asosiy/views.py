93123b6282e34e9cb87c93db01ec71cff3202106
Your Name <nurshodbekshokirov@gmail.com>
2023-02-12 11:41:02 +0500

    Forma qo'shildi

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 def reja(request):
+    if request.method == 'POST':
+        form = RejaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
     data = {
-        'reja': Reja.objects.all()
+        'reja': Reja.objects.all(),
+        'forma':RejaForm
     }
     return render(request, 'todo (2).html', data)
 
@@ -13,11 +20,9 @@ def reja_ochir(request, son):
     return redirect('/')
 
 
-def reja_qosh(request):
-    if request.method == 'POST':
-        form = RejaForm(request.POST)
-        if form.is_valid():
-            form.save()
+
+
+
 
 
 # Create your views here.
--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reja),
     path('reja/<int:son>/', reja_ochir),
+
 ]
